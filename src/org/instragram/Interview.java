d15b52574956720e389de92c32e1226b16fbe2b4
arasudurai <systeminccbe9@gmail.com>
2021-11-29 19:48:08 +0530

    replacement

---
+++ src/org/instragram/Interview.java
@@ -0,0 +1,58 @@
+package org.instragram;
+
+public class Interview {
+	
+	static int temp;
+	private static int count(int a) {
+		temp =a;
+		int d =0;
+		while (a>0) {
+			
+			int b= a/10;
+			int c = a%10;
+			a=b;
+			d=c+(d*10);
+			
+			
+		}
+		return d;		
+
+	}
+	
+	public static void main(String[] args) {
+		
+		int count = count(123411245);
+		System.out.println(count);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
