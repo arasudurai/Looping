cae64e3aa9edfca2ae3d5dbe6ab7bb9af0cc8eb4
arasudurai <systeminccbe9@gmail.com>
2021-11-29 20:04:09 +0530

    process

---
+++ src/org/instragram/Interview.java
@@ -0,0 +1,30 @@
+package org.instragram;
+
+public class Interview {
+
+	static int temp;
+	private static int count(int a) {
+
+		temp=a;
+		int d=0;
+		while (a>0) {
+			int b =a/10;
+			int c =a%10;
+			a=b;
+			d=c+(d*10);
+
+		}
+		return d;	
+
+	}
+
+	public static void main(String[] args) {
+
+		int count = count(645513);
+		System.out.println(count);
+
+
+
+	}
+
+}
